14427a7a85154f67c85a977370b9cf071af8bd30
Ben <benstevens2609@gmail.com>
2024-10-09 12:20:23 +0100

    changed NR-ANN to ANN-code

--- ANN-code/feature_extraction.py
+++ ANN-code/feature_extraction.py
@@ -2,5 +2,15 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+
+folder_path = os.path.join('..', '..', 'Data/C/300-320keV')
+files = os.listdir(folder_path)
+
+carbon_events = np.array(files)
+
+print(carbon_events)
+
+
 
 
