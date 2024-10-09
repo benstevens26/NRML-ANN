20bcd7ec5da2a0d4b26b9573bbb19a63a2b19305
Ben <benstevens2609@gmail.com>
2024-10-09 15:06:28 +0100

    plotted some events

--- ANN-code/feature_extraction.py
+++ ANN-code/feature_extraction.py
@@ -5,12 +5,14 @@ import matplotlib.pyplot as plt
 import os
 
 folder_path = os.path.join('..', '..', 'Data/C/300-320keV')
+# folder_path = os.path.join( tom path )
+
 files = os.listdir(folder_path)
+carbon_events = [np.load(folder_path + "/" + f) for f in files]
 
-carbon_events = np.array(files)
+event1 = carbon_events[0]
 
-print(carbon_events)
-
-
-
-
+plt.figure()
+plt.imshow(event1, cmap='viridis')
+plt.colorbar()
+plt.show()
