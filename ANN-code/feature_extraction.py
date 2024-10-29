844634e96176d49a0617285923b57244ee70f0e2
Ben <benstevens2609@gmail.com>
2024-10-29 11:51:10 +0000

    added extract_features

--- ANN-code/feature_extraction.py
+++ ANN-code/feature_extraction.py
@@ -1,7 +1,22 @@
 """
 """
+import numpy as np
 
+def extract_features(event, num_segments):
+    """
 
-def extract_features(event, extract_length=True):
-    raise NotImplementedError("Feature extraction is not yet implemented.")
+    :param event: event object
+    :return: array of features [length, energy, max_den, recoil_angle]
+    """
+
+    axis, mean_x, mean_y = event.get_principal_axis()
+
+    recoil_angle = event.get_recoil_angle(principal_axis=axis)
+
+    segment_distances, segment_intensities = event.get_intensity_profile(num_segments)
+    length = event.get_track_length(num_segments, segment_distances, segment_intensities)
+    energy = event.energy
+    max_den = event.get_max_den()
+
+    return np.array([length, energy, max_den, recoil_angle])
 
