bb0047090748db1453876db4c0a76a4e04ac8e54
Ben <benstevens2609@gmail.com>
2024-10-29 11:15:31 +0000

    changed analysis.py slightly

--- ANN-code/analysis.py
+++ ANN-code/analysis.py
@@ -5,16 +5,27 @@ analysis
 
 from event import Event
 from event import load_events
+import matplotlib.pyplot as plt
 import numpy as np
 import random
 import tqdm
 
 folder_path = "Data/C/300-320keV"
 events = load_events(folder_path)
+events = random.sample(events, 20)
 
-# sample some events for analysis
-events = random.sample(events, 10)
 test_event = events[0]
-test_event.plot_image_with_axis(image_type="raw")
-test_event.plot_image_with_axis()
-#test_event.plot_intensity_profile(num_segments=250)
+
+test_event.plot_image()
+
+principal_axis, mean_x, mean_y = test_event.get_principal_axis()
+print(test_event.get_recoil_angle(principal_axis=principal_axis))
+
+print(principal_axis)
+
+
+
+
+
+
+
