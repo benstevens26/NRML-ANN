a5b6e3742eba8c928c71936d7429dc3d664b12f3
Ben <benstevens2609@gmail.com>
2024-10-30 12:14:18 +0000

    added noise_adder to pipeline

--- ANN-code/event_processor.py
+++ ANN-code/event_processor.py
@@ -2,17 +2,36 @@ import numpy as np
 import scipy.ndimage as nd
 import os
 import csv
+from convert_sim_ims import *
 from tqdm import tqdm
 from event import Event
-
-
-def noise_adder(event):
-    return event
 
 
 def smooth_operator(event, smoothing_sigma=5):
     event.image = nd.gaussian_filter(event.image, sigma=smoothing_sigma)
 
+    return event
+
+def noise_adder(event, m_dark, example_dark_list):
+    """Adds noise to an array of raw images
+
+    Args:
+        event_list (array): An array containing instances of the Event class to have noise added to them. NOTE that the events should be initialised with the raw images.
+        m_dark (array): 2D array (image) containing the master dark. This is by default loaded from "Data/darks/master_dark_1x1.npy", but would change with binning.
+        example_dark_list (array): An array containing the example darks. This is by default loaded from "Data/darks/quest_std_dark_1.npy" but should really use a random one from that folder each time.
+
+    Returns:
+        array: An array of images that have had a random sample of noise added to them.
+    """
+
+    event.image = convert_im(
+            event.image,
+            get_dark_sample(
+                m_dark,
+                [len(event.image[0]), len(event.image)],
+                example_dark_list[np.random.randint(0, len(example_dark_list) - 1)],
+            ),
+        )
     return event
 
 
@@ -49,7 +68,7 @@ def event_processor(events, chunk_size, output_csv):
 
         chunk = []
 
-        for event in tqdm(events):
+        for event in events:
             event = noise_adder(event)
             event = smooth_operator(event)
             features = extract_features(event)
