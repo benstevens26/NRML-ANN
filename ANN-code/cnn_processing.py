a6ab3300a49cca593b99f7fc239b1bd44ffdb49e
Ben <benstevens2609@gmail.com>
2024-11-20 14:37:26 +0000

    dleeted a method that was skeleton

--- ANN-code/cnn_processing.py
+++ ANN-code/cnn_processing.py
@@ -7,6 +7,9 @@ contains methods for changing the event.image to be correct for use in the CNN m
 
 import cv2
 import tensorflow as tf
+import random
+import numpy as np
+
 
 def resize_pad_image_tf(event, target_size=(224, 224)):
     """
@@ -17,20 +20,32 @@ def resize_pad_image_tf(event, target_size=(224, 224)):
 
     return
 
+
 def pad_image(event, target_size=(415, 559)):
 
+    small_image = event.image
+
+    try:
+        small_height, small_width = small_image.shape[:2]
+        target_height, target_width = target_size
+
+        # Create an empty frame filled with zeros (black) of size (415, 559)
+        target_frame = np.zeros((target_height, target_width), dtype=small_image.dtype)
+
+        # Calculate maximum offsets so the small image fits inside the target frame
+        max_y_offset = target_height - small_height
+        max_x_offset = target_width - small_width
+
+        # Generate random offsets within the allowable range
+        y_offset = random.randint(0, max_y_offset)
+        x_offset = random.randint(0, max_x_offset)
+
+        # Insert the small image into the target frame at the random offset
+        target_frame[y_offset:y_offset + small_height, x_offset:x_offset + small_width] = small_image
+
+        event.image = target_frame
+
+    except:
+        "Image could not fit inside target frame"
 
 
-
-
-def resize_event_image(event, res=None):
-    """
-
-    :param event: event object
-    :param res: resolution desired, defaults to None (will resize to the maximum)
-    :return:
-    """
-
-    if res is None:
-
-        return
