cfc369ef2d3ee4a4d40a7167e31b70b33df98472
Ben <benstevens2609@gmail.com>
2024-12-02 18:10:42 +0000

    checking SSH physical devices

--- ANN-code/train_cnn.py
+++ ANN-code/train_cnn.py
@@ -10,11 +10,13 @@ from cnn_processing import bin_image, smooth_operator, noise_adder, pad_image, p
 
 # Define base directories and batch size
 
-base_dirs = ['/vols/lz/MIGDAL/sim_ims/C', '/vols/lz/MIGDAL/sim_ims/F']  # List your data directories here
+# base_dirs = ['/vols/lz/MIGDAL/sim_ims/C', '/vols/lz/MIGDAL/sim_ims/F']  # List your data directories here
+base_dirs = ['Data/C', 'Data/F']  # List your data directories here
 batch_size = 32
 dark_list_number = 0
 binning = 1
-dark_dir="/vols/lz/MIGDAL/sim_ims/darks"
+# dark_dir="/vols/lz/MIGDAL/sim_ims/darks"
+dark_dir="Data/darks"
 m_dark = np.load(f"{dark_dir}/master_dark_{str(binning)}x{str(binning)}.npy")
 example_dark_list_unbinned = np.load(
     f"{dark_dir}/quest_std_dark_{dark_list_number}.npy"
@@ -34,20 +36,37 @@ val_dataset = remaining.take(val_size)  # Next 15%
 test_dataset = remaining.skip(val_size)  # Final 15%
 
 # Define the model
+# CoNNCR = tf.keras.Sequential([
+#     tf.keras.layers.Input(shape=(415, 559, 1)),
+#     tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+#     tf.keras.layers.MaxPooling2D((2, 2)),
+#     tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+#     tf.keras.layers.MaxPooling2D((2, 2)),
+#     tf.keras.layers.Flatten(),
+#     tf.keras.layers.Dense(128, activation='relu'),
+#     tf.keras.layers.Dropout(0.5),
+#     tf.keras.layers.Dense(2, activation='softmax')
+# ])
+
 CoNNCR = tf.keras.Sequential([
     tf.keras.layers.Input(shape=(415, 559, 1)),
     tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D((2, 2)),
     tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D((2, 2)),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.GlobalAveragePooling2D(),
+    tf.keras.layers.Dense(64, activation='relu'),
     tf.keras.layers.Dropout(0.5),
     tf.keras.layers.Dense(2, activation='softmax')
 ])
 
 # Compile the model
 CoNNCR.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+print(CoNNCR.summary())
+print(tf.config.list_physical_devices())
+
+exit()
 
 # Train the model
 CoNNCR.fit(train_dataset, validation_data=val_dataset, epochs=20)
