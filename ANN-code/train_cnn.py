b72cb3ff35b0607f475d892776ddaac273dcc965
TeeVahReeeeeen <tvarine@googlemail.com>
2024-12-04 14:45:39 +0000

    commented out thing

--- ANN-code/train_cnn.py
+++ ANN-code/train_cnn.py
@@ -10,6 +10,10 @@ from cnn_processing import bin_image, smooth_operator, noise_adder, pad_image, p
 
 # Define base directories and batch size
 
+# print(tf.config.list_physical_devices())
+
+# exit()
+
 base_dirs = ['/vols/lz/MIGDAL/sim_ims/C', '/vols/lz/MIGDAL/sim_ims/F']  # List your data directories here
 # base_dirs = ['Data/C', 'Data/F']  # List your data directories here
 batch_size = 32
@@ -36,29 +40,29 @@ val_dataset = remaining.take(val_size)  # Next 15%
 test_dataset = remaining.skip(val_size)  # Final 15%
 
 # Define the model
-# CoNNCR = tf.keras.Sequential([
-#     tf.keras.layers.Input(shape=(415, 559, 1)),
-#     tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
-#     tf.keras.layers.MaxPooling2D((2, 2)),
-#     tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-#     tf.keras.layers.MaxPooling2D((2, 2)),
-#     tf.keras.layers.Flatten(),
-#     tf.keras.layers.Dense(128, activation='relu'),
-#     tf.keras.layers.Dropout(0.5),
-#     tf.keras.layers.Dense(2, activation='softmax')
-# ])
-
 CoNNCR = tf.keras.Sequential([
     tf.keras.layers.Input(shape=(415, 559, 1)),
     tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D((2, 2)),
     tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D((2, 2)),
-    tf.keras.layers.GlobalAveragePooling2D(),
-    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dropout(0.5),
     tf.keras.layers.Dense(2, activation='softmax')
 ])
+
+# CoNNCR = tf.keras.Sequential([
+#     tf.keras.layers.Input(shape=(415, 559, 1)),
+#     tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+#     tf.keras.layers.MaxPooling2D((2, 2)),
+#     tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+#     tf.keras.layers.MaxPooling2D((2, 2)),
+#     tf.keras.layers.GlobalAveragePooling2D(),
+#     tf.keras.layers.Dense(64, activation='relu'),
+#     tf.keras.layers.Dropout(0.5),
+#     tf.keras.layers.Dense(2, activation='softmax')
+# ])
 
 # Compile the model
 CoNNCR.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
