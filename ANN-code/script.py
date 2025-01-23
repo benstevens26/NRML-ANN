ad81d7d5ed16bcbb79097a9df63eea42d6fc0b4a
Ben <benstevens2609@gmail.com>
2025-01-23 19:47:53 +0000

    modified script

--- ANN-code/script.py
+++ ANN-code/script.py
@@ -19,6 +19,8 @@ im_dirs = [
 
 max_x = 0
 max_y = 0
+biggest_image_x = None
+biggest_image_y = None
 
 for dir in im_dirs:
     for filename in tqdm(os.listdir(dir)):
@@ -31,7 +33,12 @@ for dir in im_dirs:
 
             if x > max_x:
                 max_x = x
+                biggest_image_x = full_path
+                
             if y > max_y:
                 max_y = y
+                biggest_image_y = full_path
 
 print(max_x, max_y)
+print("the biggest x image",biggest_image_x)
+print("the biggest y image",biggest_image_y)
