bd708a03bbbad9f49d55a89563f7825de2987936
Ben <benstevens2609@gmail.com>
2025-01-25 22:27:45 +0000

    updated script.py

--- ANN-code/script.py
+++ ANN-code/script.py
@@ -22,10 +22,12 @@ im_dirs = [
     "/vols/lz/tmarley/GEM_ITO/run/im4/F",
 ]
 
+
+# check all images in the directory (end in .npy)
 min_dim_error = []
 uncropped_error = []
 
-search_fraction = 0.8
+# ssearch criteria
 min_dim = 20
 
 for im_dir in im_dirs:
@@ -38,19 +40,18 @@ for im_dir in im_dirs:
                 min_dim_error.append(im_file)
                 continue
 
-            if uncropped_check(image, search_fraction, method="max_comparison"):
+            if uncropped_check(image, search_fraction=0.8, method='max_comparison'):
                 uncropped_error.append(im_file)
                 continue
 
-            if uncropped_check(image, search_fraction, method="area_comparison"):
+            if uncropped_check(image, search_fraction=0.5, method='area_comparison'):
                 uncropped_error.append(im_file)
+                
 
-
-print(
-    "Number of images that violate the minimum dimension criteria: ", len(min_dim_error)
-)
+print("Number of images that violate the minimum dimension criteria: ", len(min_dim_error))
 print("Number of images that violate the uncropped criteria: ", len(uncropped_error))
 
 # save the min_dim_error and uncropped_error lists as csv files
 np.savetxt("min_dim_error.csv", min_dim_error, delimiter=",", fmt="%s")
 np.savetxt("uncropped_error.csv", uncropped_error, delimiter=",", fmt="%s")
+
