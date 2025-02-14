50fba650b2b402a1b9c4f0d0008553b1405d867f
Ben <benstevens2609@gmail.com>
2025-02-14 16:24:35 +0000

    clear script

--- ANN-code/script.py
+++ ANN-code/script.py
@@ -2,19 +2,3 @@
 """
 SSH SCRIPT
 """
-
-import pandas as pd
-
-# List to hold dataframes
-dfs = []
-
-# Read in the CSV files
-for i in range(15):
-    df = pd.read_csv(f'features_Ar_CF4_{i}.csv')
-    dfs.append(df)
-
-# Concatenate all dataframes
-concatenated_df = pd.concat(dfs, ignore_index=True)
-
-# Save the concatenated dataframe to a new CSV file
-concatenated_df.to_csv('features_Ar_CF4.csv', index=False)
