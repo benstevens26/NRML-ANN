c8ad5a006b787cf8cdd438cf5b40c4bc894386ef
Ben <benstevens2609@gmail.com>
2025-02-15 13:56:11 +0000

    update LENRI_CF4 model with new tuned hyperparameters

--- ANN-code/model.py
+++ ANN-code/model.py
@@ -4,20 +4,18 @@ model.py - Neural Network for Low Energy Nuclear Recoil Investigation (LENRI)
 
 import torch.nn as nn
 
-class LENRI(nn.Module):
-    def __init__(self, input_size=9):
-        super(LENRI, self).__init__()
+class LENRI_CF4(nn.Module):
+    def __init__(self, input_size=9, dropout_rate=0.106):
+        super(LENRI_CF4, self).__init__()
         self.model = nn.Sequential(
-            nn.Linear(input_size, 32),
+            nn.Linear(input_size, 65),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.Dropout(0.2),
-            nn.Linear(32, 16),
+            nn.Dropout(dropout_rate),
+            nn.Linear(65, 71),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.Dropout(0.2),
-            nn.Linear(16, 8),
-            nn.LeakyReLU(negative_slope=0.01),
-            nn.Linear(8, 2),  
+            nn.Dropout(dropout_rate),
+            nn.Linear(71, 2)
         )
-
+    
     def forward(self, x):
         return self.model(x)
