63e45620e72869a0c84562f508078a0d4c5f5097
Ben <benstevens2609@gmail.com>
2025-02-17 14:48:15 +0000

    add class LENRI_Ar_CF4

--- ANN-code/model.py
+++ ANN-code/model.py
@@ -5,17 +5,38 @@ model.py - Neural Network for Low Energy Nuclear Recoil Investigation (LENRI)
 import torch.nn as nn
 
 class LENRI_CF4(nn.Module):
-    def __init__(self, input_size=9, dropout_rate=0.106):
+    def __init__(self, input_size=9, dropout_rate=0.0864):
         super(LENRI_CF4, self).__init__()
         self.model = nn.Sequential(
-            nn.Linear(input_size, 65),
+            nn.Linear(input_size, 96),
             nn.LeakyReLU(negative_slope=0.01),
             nn.Dropout(dropout_rate),
-            nn.Linear(65, 71),
+            nn.Linear(96, 62),
             nn.LeakyReLU(negative_slope=0.01),
             nn.Dropout(dropout_rate),
-            nn.Linear(71, 2)
+            nn.Linear(62, 2)
         )
     
     def forward(self, x):
         return self.model(x)
+
+
+class LENRI_Ar_CF4(nn.Module):
+    def __init__(self, input_size=9, dropout_rate=0.0864):
+        super(LENRI_Ar_CF4, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(input_size, 128),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Dropout(dropout_rate),
+            nn.Linear(128, 86),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Dropout(dropout_rate),
+            nn.Linear(86, 48),
+            nn.LeakyReLU(negative_slope=0.01),
+            nn.Dropout(dropout_rate),
+            nn.Linear(48, 3)
+        )
+    
+    def forward(self, x):
+        return self.model(x)
+
