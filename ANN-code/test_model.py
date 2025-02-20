5676a1b29657d7b8590ac634f4cf398bf9b4cc57
Ben <benstevens2609@gmail.com>
2025-02-20 17:02:23 +0000

    Add multi-class test functionality

--- ANN-code/test_model.py
+++ ANN-code/test_model.py
@@ -13,16 +13,16 @@ import torch
 import torch.nn as nn
 from sklearn.metrics import precision_score, recall_score, f1_score
 from feature_preprocessing import get_dataloaders_cf4, get_dataloaders_ar_cf4
-from model import LENRI_CF4, LENRI_Ar_CF4
+from models import LENRI_CF4_1, LENRI_Ar_CF4_1, LENRI_Ar_CF4_1_opt
 
 # Device configuration
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
-model_path = "LENRI_CF4_old_unopt.pth"
-features_path = "ANN-code/Data/features_old/all_features_2_scaled.csv"
-model = LENRI_CF4().to(device)
-binary = True
+model_path = "lenri_Ar_CF4_1_opt.pth"
+features_path = "ANN-code/Data/features_Ar_CF4_processed.csv"
+model = LENRI_Ar_CF4_1_opt().to(device)
+binary = False
 
 # Load test set
 if binary:
@@ -64,14 +64,27 @@ with torch.no_grad():
 test_loss /= len(test_loader)
 test_acc = correct_test / total_test
 
-# Compute additional metrics
-precision = precision_score(all_labels, all_preds)
-recall = recall_score(all_labels, all_preds)
-f1 = f1_score(all_labels, all_preds)
-
-# Print results
 print(f"Test Loss: {test_loss:.4f}")
 print(f"Test Accuracy: {test_acc:.4f}")
-print(f"Precision: {precision:.4f}")
-print(f"Recall: {recall:.4f}")
-print(f"F1 Score: {f1:.4f}")
+
+# Compute additional metrics
+if binary:
+    precision = precision_score(all_labels, all_preds)
+    recall = recall_score(all_labels, all_preds)
+    f1 = f1_score(all_labels, all_preds)
+    # Print results
+    print(f"Precision: {precision:.4f}")
+    print(f"Recall: {recall:.4f}")
+    print(f"F1 Score: {f1:.4f}")
+else:
+    precision_per_class = precision_score(all_labels, all_preds, average=None)
+    recall_per_class = recall_score(all_labels, all_preds, average=None)
+    f1_per_class = f1_score(all_labels, all_preds, average=None)
+    print('[Carbon, Fluorine, Argon]')
+    print(f"Precision per class: {precision_per_class}")
+    print(f"Recall per class: {recall_per_class}")
+    print(f"F1 Score per class: {f1_per_class}")
+
+
+
+
