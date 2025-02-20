1908c590bb42920086fa628ce552326ec83e82b8
Ben <benstevens2609@gmail.com>
2025-02-20 17:03:54 +0000

    Change model.py to models.py

--- ANN-code/models.py
+++ ANN-code/models.py
@@ -4,39 +4,45 @@ model.py - Neural Network for Low Energy Nuclear Recoil Investigation (LENRI)
 
 import torch.nn as nn
 
-
-class LENRI_CF4(nn.Module):
-    def __init__(self, input_size=9, dropout_rate=0.0864):
-        super(LENRI_CF4, self).__init__()
+class LENRI_CF4_1(nn.Module):
+    """
+    LENRI hyperparameter tuned model for CF4-1 dataset.
+    """
+    def __init__(self, input_size=9):
+        super(LENRI_CF4_1, self).__init__()
         self.model = nn.Sequential(
             nn.Linear(input_size, 96),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.Dropout(dropout_rate),
+            nn.Dropout(p=0.0864),
             nn.Linear(96, 62),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.Dropout(dropout_rate),
+            nn.Dropout(p=0.0864),
             nn.Linear(62, 2),
         )
 
     def forward(self, x):
         return self.model(x)
+    
 
-
-class LENRI_Ar_CF4(nn.Module):
-    def __init__(self, input_size=9, dropout_rate=0.0864):
-        super(LENRI_Ar_CF4, self).__init__()
+class LENRI_Ar_CF4_1(nn.Module):
+    """
+    LENRI hyperparameter tuned model for Ar-CF4-1 dataset.
+    """
+    def __init__(self, input_size=9):
+        super(LENRI_Ar_CF4_1, self).__init__()
         self.model = nn.Sequential(
-            nn.Linear(input_size, 128),
+            nn.Linear(input_size, 117),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.Dropout(dropout_rate),
-            nn.Linear(128, 86),
+            nn.Dropout(p=0.0693),
+            nn.Linear(117, 40),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.Dropout(dropout_rate),
-            nn.Linear(86, 48),
+            nn.Dropout(p=0.0693),
+            nn.Linear(40, 48),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.Dropout(dropout_rate),
+            nn.Dropout(p=0.0693),
             nn.Linear(48, 3),
         )
 
     def forward(self, x):
         return self.model(x)
+    
